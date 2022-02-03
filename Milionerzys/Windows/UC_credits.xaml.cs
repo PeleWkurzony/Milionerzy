8d21a337ccc9ffc297dc60386bedb26691bc1489
Pele <banskimateusz7@gmail.com>
2022-02-03 13:36:58 +0100

    Revert "Revert "Final version""
    This reverts commit 929ec5c14978c7b7778951c83fa7583ecb58df08.

---
+++ Milionerzys/Windows/UC_credits.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Milionerzy.Windows {
+    /// <summary>
+    /// Logika interakcji dla klasy UC_credits.xaml
+    /// </summary>
+    public partial class UC_credits : UserControl {
+        MainWindow parent;
+        public UC_credits(MainWindow parent) {
+            InitializeComponent();
+            this.parent = parent;
+        }
+        private void ui_back_button_Click(object sender, RoutedEventArgs e) {
+            parent.ReturnToMenu();
+        }
+    }
+}
---
+++ Milionerzys/Windows/UC_start_game.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Milionerzy.Windows {
+    /// <summary>
+    /// Logika interakcji dla klasy UC_start_game.xaml
+    /// </summary>
+    public partial class UC_start_game : UserControl {
+
+        MainWindow parent;
+        public UC_start_game(MainWindow parent) {
+            InitializeComponent();
+            this.parent = parent;
+        }
+
+        private void ui_start_game_Click(object sender, RoutedEventArgs e) {
+            if (ui_nickname.Text == "") {
+                ui_nickname.BorderBrush = new SolidColorBrush(Color.FromRgb(191, 25, 0));
+                ui_nickname.BorderThickness = new Thickness(2);
+            }
+            else {
+                parent.SwitchTo(parent.UCgame);
+            }
+            
+        }
+
+        private void ui_back_button_Click(object sender, RoutedEventArgs e) {
+            parent.ReturnToMenu();
+        }
+    }
+}
