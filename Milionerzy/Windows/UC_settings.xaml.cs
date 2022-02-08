a1e14393c529f6f889aa6302ff062dfb80144a85
Pele <banskimateusz7@gmail.com>
2022-02-08 09:29:08 +0100

    Change audio to disabled

--- Milionerzy/Windows/UC_settings.xaml.cs
+++ Milionerzy/Windows/UC_settings.xaml.cs
@@ -21,43 +21,32 @@ namespace Milionerzy.Windows {
 
         MainWindow parent;
         public UC_settings(MainWindow parent) {
-            try {
                 this.parent = parent;
+                this.parent.mp4.IsMuted = true;
                 InitializeComponent();
-            } catch { }
         }
 
         private void ui_back_button_Click(object sender, RoutedEventArgs e) {
-            this.Dispatcher.Invoke(() => {
                 parent.ReturnToMenu();
-            });
         }
         private void ui_database_saving_checkbox_Click(object sender, RoutedEventArgs e) {
-            this.Dispatcher.Invoke(() => {
                 parent.DBcontroller.ChangeSaving(ui_database_saving_checkbox.IsChecked ?? true);
-            });
         }
 
         private void ui_audio_checkbox_Click(object sender, RoutedEventArgs e) {
-            this.Dispatcher.Invoke(() => {
                 parent.mp4.IsMuted = !ui_audio_checkbox.IsChecked ?? false;
-            });
         }
 
         private void ui_backgorund_checkbox_Click(object sender, RoutedEventArgs e) {
-            this.Dispatcher.Invoke(() => {
                 bool flag = !ui_backgorund_checkbox.IsChecked ?? false;
                 if (flag) {
                     parent.mp4.Pause();
                 } else {
                     parent.mp4.Play();
-                }
-            });
-            
+                }  
         }
 
         private void ui_save_settings_Click(object sender, RoutedEventArgs e) {
-            this.Dispatcher.Invoke(() => {
                 parent.DBcontroller.ChangeConnectionString(
                     ui_address.Text,
                     ui_user.Text,
@@ -65,7 +54,6 @@ namespace Milionerzy.Windows {
                     ui_port.Text,
                     ui_password.Text);
                 parent.DBcontroller.DownloadQuestions();
-            });
         }
     }
 }
